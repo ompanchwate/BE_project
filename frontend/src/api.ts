9df1e85d4086bba3339ec4b5f0ecc864cbac8092
Om Panchwate <86113092+ompanchwate@users.noreply.github.com>
2025-06-11 21:51:56 +0530

    Update api.ts

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://127.0.0.1:5000';
+const API_URL = 'https://handytalk-backend.vercel.app/';
 
 export const signinUser = async (formData) => {
   return axios.post(`${API_URL}/signin`, formData);
