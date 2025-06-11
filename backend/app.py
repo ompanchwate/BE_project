ab178825f7a0a6150a9f6a854f2778d54abd3863
ompanchwate <ompanchwate2003@gmail.com>
2025-06-12 01:19:45 +0530

    app.py updated

--- backend/app.py
+++ backend/app.py
@@ -28,13 +28,6 @@ app = Flask(__name__)
 
 CORS(app, supports_credentials=True)
 
-@app.after_request
-def add_cors_headers(response):
-    response.headers['Access-Control-Allow-Origin'] = 'https://handytalk.vercel.app'
-    response.headers['Access-Control-Allow-Headers'] = 'Content-Type,Authorization'
-    response.headers['Access-Control-Allow-Methods'] = 'GET,POST,PUT,DELETE,OPTIONS'
-    response.headers['Access-Control-Allow-Credentials'] = 'true'
-    return response
 
 bcrypt = Bcrypt(app)
 
